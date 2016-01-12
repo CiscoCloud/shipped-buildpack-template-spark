75c087110cf733e63f90271ca9c7a3a334b3db78
Vijay Poojary <vj.scjp@gmail.com>
2016-01-12 14:25:03 -0500

    minimizing dependencies

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,12 @@
 import express from 'express';
 import ConnectSpark from './connectSpark';
 
-/* eslint new-cap: 0 */
 const router = express.Router();
-
-/* GET home page. */
-/* eslint-disable no-unused-vars */
-router.get('/', (req, res, next) => {
-  /* eslint-enable no-unused-vars */
-  res.render('index', { title: 'Express' });
-});
 
 const routes = ["rooms", "messages", "memberships", "people", "webhooks"];
 
 routes.forEach((route) =>{
+  console.log("route:" , route);
   router.all("/" + route + "*", (req, res, next) => {
     ConnectSpark(req, res);
   });
